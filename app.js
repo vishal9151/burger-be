f16f757281a62caf0fb16a1739d6e913f4827d70
vish9151 <84173784+vishal9151@users.noreply.github.com>
2023-10-30 19:56:13 +0530

    updated

---
+++ app.js
@@ -0,0 +1,59 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import { connectPassport } from "./utils/provider.js";
+import session from "express-session";
+import passport from "passport";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middlewares/errorMiddlewares.js";
+
+import cors from "cors";
+
+const app = express();
+
+dotenv.config({
+  path: "./config/config.env",
+});
+
+// Using middlewares
+app.use(
+  session({
+    secret: process.env.SECRET_Id,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {domain: '.vishalmangla.tech'},
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Initialize Passport and configure
+app.use(passport.authenticate("session"));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      "https://burger-wala.vishalmangla.tech",
+      "http://localhost:3000",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+import userRoute from "./routes/user.js";
+import orderRoute from "./routes/orders.js";
+
+// Initialize Passport and configure it
+connectPassport();
+
+app.use("/api/v1", userRoute);
+app.use("/api/v1", orderRoute);
+
+app.use(errorMiddleware);
+
+export default app;
